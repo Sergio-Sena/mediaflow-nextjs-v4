28eb3053226a512bb92eb8b75db08ceb7155d61c
Sergio Sena <senanetworker@gmail.com>
2026-03-03 12:02:40 -0300

    fix: Otimizar tamanhos de botões para landscape mobile
    - Breakpoint landscape customizado (orientation:landscape + max-height:500px)
    
    - Play button landscape: p-1.5, w-4 h-4
    
    - Skip buttons landscape: p-0.5, w-3 h-3
    
    - Gap reduzido: gap-1.5
    
    - Testado em Poco F7

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'landscape': {'raw': '(orientation: landscape) and (max-height: 500px)'},
+      },
       colors: {
         neon: {
           cyan: '#00ffff',
